5d62e50a9ff5b6cd30443ea9702ea155e2e5725e
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-01-15 01:55:36 -0300

    Package it as a nix flake.

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A simple launcher for desktop applications";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells = rec {
+        default = pkgs.mkShell {
+          packages = [ pkgs.dex ];
+        };
+      };
+
+      packages = rec {
+        desktop-entry-launcher = pkgs.writeScriptBin "desktop-entry-launcher" ''
+          export PATH="${pkgs.lib.makeBinPath [ pkgs.dex ]}:$PATH"
+          ${./desktop-entry-launcher} $@
+        '';
+
+        default = desktop-entry-launcher;
+      };
+
+
+      apps = rec {
+        desktop-entry-launcher = flake-utils.lib.mkApp {
+          drv = self.packages.${system}.desktop-entry-launcher;
+        };
+        default = desktop-entry-launcher;
+      };
+    }
+  );
+}
